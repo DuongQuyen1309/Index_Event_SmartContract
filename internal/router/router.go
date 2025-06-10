f9502c5d12d67785c748be92f427b090ee052061
DuongQuyen1309 <minhquyen130904@gmail.com>
2025-06-10 10:46:16 +0700

    fix for router

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,8 +9,8 @@ func SetupRouer() *gin.Engine {
 	router := gin.Default()
 	router.GET("/users/:address/turn-amount", handler.GetTotalTurnAmountOfUser)
 	router.GET("/users/:address/turn-requests", handler.GetTurnsRequestsOfUser)
-	router.GET("/turn-requests/:hash", handler.GetTurnRequestByHash)
-	router.GET("/turn-requests/:hash/prizes", handler.GetPrizesOfHash)
+	router.GET("/turn-request/:hash", handler.GetTurnRequestByHash)
+	router.GET("/turn-request/:hash/prizes", handler.GetPrizesOfHash)
 	return router
 
 }
